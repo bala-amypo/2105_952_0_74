e2b52c248a8587b249591908885ccc4d0c2de473
bala-amypo <bala@amypo.in>
2025-12-17 09:33:35 +0000

    User commit

--- src/main/java/com/example/demo/entity/Student.java
+++ src/main/java/com/example/demo/entity/Student.java
@@ -1,49 +1,59 @@
 package com.example.demo.entity;
+
 import java.time.LocalDate;
+
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 
 @Entity
-//@Table(name="StudentTable")
 public class Student {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id ;
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
     private String name;
-    private String email;
-    
-    
+    private String dept;
+    private LocalDate dob;
+    private float cgpa;
+    public Long getId(){
+        return id;
+    }
     public void setId(Long id){
         this.id=id;
-
     }
-    public Long getId() {
-        return id;
-        
-    }
-    public String getName() {
+    public String getName(){
         return name;
     }
-    public void setName(String name) {
-        this.name = name;
+    public void setName(String name){
+        this.name=name;
     }
-    public String getEmail() {
-        return email;
+    public String getDept(){
+        return dept;
     }
-    public void setEmail(String email) {
-        this.email = email;
+    public void setDept(String dept){
+        this.dept=dept;
     }
-    
-    public Student(Long id, String name, String email) {
-        this.id = id;
-        this.name = name;
-        this.email = email;
-       
+    public LocalDate getDob(){
+        return dob;
     }
-    public Student() {
+    public void setDob(LocalDate dob){
+        this.dob=dob;
     }
+    public float getCgpa(){
+        return cgpa;
+    }
+    public void setCgpa(float cgpa){
+        this.cgpa=cgpa;
+    }
+    public Student(){
 
-
+    }
+    public Student(Long id,String name,String dept,LocalDate dob,float cgpa){
+        this.id=id;
+        this.name=name;
+        this.dept=dept;
+        this.dob=dob;
+        this.cgpa=cgpa;
+    }
 }
--- src/main/java/com/example/demo/repository/StudentRepositoty.java
+++ src/main/java/com/example/demo/repository/StudentRepositoty.java
@@ -1,10 +1,10 @@
-package com.example.aiml.repository;
+package com.example.demo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-import com.example.aiml.entity.Student;
-
+import com.example.demo.entity.Student;
 @Repository
-public interface StudentRepository extends JpaRepository <Student,Long>{
-    
+public interface StudentRepository extends JpaRepository<Student,Long>{
+
 }
